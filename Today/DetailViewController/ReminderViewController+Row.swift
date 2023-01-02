ad545e40d3ff4d1db81c16ad19338be8de8319df
Ольга Егорова <hardcorecase@gmail.com>
2023-01-02 18:05:15 +0300

    enum для настройки компонентов ячейки

---
+++ Today/DetailViewController/ReminderViewController+Row.swift
@@ -0,0 +1,44 @@
+//
+//  ReminderViewController+Row.swift
+//  Today
+//
+//  Created by Ольга Егорова on 31.12.2022.
+//
+
+import Foundation
+import UIKit
+
+extension ReminderViewController{
+    enum Row: Hashable {
+        case viewDate
+        case viewNotes
+        case viewTime
+        case viewTitle
+        
+        var imageName: String? {
+            switch self {
+            case .viewDate:
+                return "calendar.circle"
+            case .viewNotes:
+                return "square.and.pencil"
+            case .viewTime:
+                return "clock"
+            default: return nil
+            }
+        }
+        
+        var image: UIImage? {
+            guard let imageName = imageName else  {return nil}
+            let configuration = UIImage.SymbolConfiguration(textStyle: .headline)
+            return UIImage(systemName: imageName, withConfiguration: configuration)
+                
+        }
+        var textStyle: UIFont.TextStyle {
+            switch self {
+            case .viewTitle: return .headline
+            default: return .subheadline
+            }
+        }
+        
+    }
+}
---
+++ Today/DetailViewController/ReminderViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReminderViewController.swift
+//  Today
+//
+//  Created by Ольга Егорова on 31.12.2022.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+    var reminder: Reminder
+    
+    init(reminder: Reminder) {
+        self.reminder = reminder
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfiguration.showsSeparators = false
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        super.init(collectionViewLayout: listLayout)
+        
+    }
+    
+    required init?(coder: NSCoder){
+        fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+    
+    func text(for row: Row) -> String? {
+        switch row {
+        case .viewDate: return reminder.dueDate.dayText
+        case .viewNotes: return reminder.notes
+        case .viewTime: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .viewTitle: return reminder.title
+        }
+    }
+   
+}
