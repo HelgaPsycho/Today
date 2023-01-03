f44c9e297df92c7c925c40166b913ab406c303eb
Ольга Егорова <hardcorecase@gmail.com>
2023-01-03 10:00:08 +0300

    настройка datasource

---
+++ Today/Colors ext.swift
@@ -0,0 +1,13 @@
+//
+//  Colors ext.swift
+//  Today
+//
+//  Created by Ольга Егорова on 03.01.2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var todayPrimaryTint = UIColor(named: "todayPrimaryTint")
+}
