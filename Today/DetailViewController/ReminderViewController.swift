9ac32f3bdab6f635a510aa60a28c834ad064cb09
Ольга Егорова <hardcorecase@gmail.com>
2023-01-08 18:12:33 +0300

    настройка режимов просмотра и редакции

--- Today/DetailViewController/ReminderViewController.swift
+++ Today/DetailViewController/ReminderViewController.swift
@@ -36,19 +36,32 @@ class ReminderViewController: UICollectionViewController {
         
         navigationItem.title = NSLocalizedString("Reminder", comment: "Reminder view controller title")
         
-        updateSnapshot()
+        updateSnapshotForViewing()
     }
     
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row){
-        var contentConfiguration = cell.defaultContentConfiguration()
-        contentConfiguration.text = text(for: row)
-        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
-        contentConfiguration.image = row.image
-        cell.contentConfiguration = contentConfiguration
+        let section = section(for: indexPath)
+        switch (section, row) {
+        case(.view, _):
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = text(for: row)
+            contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+            contentConfiguration.image = row.image
+            cell.contentConfiguration = contentConfiguration
+            
+        default:
+            fatalError("Unexpected combination of section and row.")
+        }
         cell.tintColor = .todayPrimaryTint
     }
     
-    func updateSnapshot() {
+    func updateSnapshotForEditing() {
+        var snapshot = SnapShot()
+        snapshot.appendSections([.title, .date, .notes])
+        dataSourse.apply(snapshot)
+    }
+    
+    func updateSnapshotForViewing() {
         var snapshot = SnapShot()
         snapshot.appendSections([.view])
         snapshot.appendItems([.viewTitle, .viewDate, .viewTime, .viewNotes],  toSection: .view)
