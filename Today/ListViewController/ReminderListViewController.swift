ab7e8f009ecfec5a22435ad0961dd7f8f1e31906
Ольга Егорова <hardcorecase@gmail.com>
2023-01-08 15:36:44 +0300

    добавлена навигация между двумя контроллерами с помощью navigationController

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -29,6 +29,19 @@ class ReminderListViewController: UICollectionViewController {
         
         collectionView.dataSource = dataSourse
   }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        let id = reminders[indexPath.item].id
+        showDetail(for: id)
+        return false
+    }
+    
+    func showDetail(for id: Reminder.ID){
+        let reminder = reminder(for: id)
+        let viewController = ReminderViewController(reminder: reminder)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
 
     
     private func listLayot() -> UICollectionViewCompositionalLayout {
